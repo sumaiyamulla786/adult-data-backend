ebf90220767a6964c2fdf3bf5582b4d36f7967dd
Sumaiya Mulla <sumaiya@getolly.com>
2018-12-21 23:06:47 +0530

    use redis for caching

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, g, jsonify, request
+from flask import Flask, g, request
 from pymongo import MongoClient
 from helpers.helpers import getPersonsCount
-from bson.json_util import dumps
+from bson.json_util import dumps, loads
 from flask_cors import CORS
+import redis
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -10,8 +12,12 @@ CORS(app)
 def connect_db():
     return MongoClient("localhost", 27017)
 
+def connect_redis():
+    return redis.StrictRedis(host="localhost", port=6379, db=0)
+
 @app.before_request
 def before_request():
+    g.redis_db = connect_redis()
     g.db = connect_db()
 
 @app.teardown_request
@@ -21,9 +27,14 @@ def teardown_request(exception):
 
 @app.route('/getCount', methods=['GET'])
 def getCount():
-    db = g.db.adultDataDB
-    persons = db.person
-    res = getPersonsCount(persons)
+    res = g.redis_db.get('getCount')
+    if(not res):
+        db = g.db.adultDataDB
+        persons = db.person
+        res = getPersonsCount(persons)
+        g.redis_db.set('getCount', json.dumps(res))
+    else:
+        res = json.loads(res)
     return dumps(res)
 
 @app.route('/persons', methods=['GET'])
@@ -46,12 +57,25 @@ def getPersons():
             filter["race"] = race
         if(relationship):
             filter["relationship"] = relationship
-    res = {}
-    res["data"] = list(persons.find(filter, None, skip, limit))
-    res["totalPages"] = int(persons.count_documents(filter)/50)
-    res['nextPage'] =  (int(page) + 1) if(int(page) + 1 <= res["totalPages"]) else -1
-    res['previousPage'] = (int(page) - 1) if(int(page) - 1 > 0) else -1
-    return dumps(res)
+    if(page == '1' and not sex and not race and not relationship):
+        res = g.redis_db.get('initialPage')
+        if(not res):
+            res = {}
+            res["data"] = list(persons.find(filter, None, skip, limit))
+            res["totalPages"] = int(persons.count_documents(filter)/50)
+            res['nextPage'] =  (int(page) + 1) if(int(page) + 1 <= res["totalPages"]) else -1
+            res['previousPage'] = (int(page) - 1) if(int(page) - 1 > 0) else -1
+            g.redis_db.set('initialPage', dumps(res))
+        else:
+            res = loads(res)
+        return dumps(res)
+    else:
+        res = {}
+        res["data"] = list(persons.find(filter, None, skip, limit))
+        res["totalPages"] = int(persons.count_documents(filter)/50)
+        res['nextPage'] =  (int(page) + 1) if(int(page) + 1 <= res["totalPages"]) else -1
+        res['previousPage'] = (int(page) - 1) if(int(page) - 1 > 0) else -1
+        return dumps(res)
 
 if __name__ == "__main__":
     app.run(debug=True)
